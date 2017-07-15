1f8ec302de70f6e1d159fdc34334eebd233c94a5
Pradeep Joshi <pradeep.joshi@Pradeep-Joshi.local>
2017-07-15 15:10:13 +0530

    first commit

---
+++ login/includes/psl-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "unlimited700");    // The database username. 
+define("PASSWORD", "785785");    // The database password. 
+define("DATABASE", "phpmyadmin");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
